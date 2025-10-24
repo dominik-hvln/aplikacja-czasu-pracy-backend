0c4e581264a44c1187b1569c1f063cbc0a68feff
Dominik Kowalski <dominik@dkowalski.pl>
2025-10-24 22:27:01 +0200

    task manager

--- src/activity/activity.module.ts
+++ src/activity/activity.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ActivityController } from './activity.controller';
 import { ActivityService } from './activity.service';
+import { SupabaseModule } from '../supabase/supabase.module';
 
 @Module({
+    imports: [SupabaseModule],
   controllers: [ActivityController],
   providers: [ActivityService]
 })
