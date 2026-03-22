81ea461b56d5a3988edd580fe8efc720573b6f73
Dominik Kowalski <dominik@dkowalski.pl>
2026-03-22 22:38:13 +0100

    absences settings

--- src/absences/absences.module.ts
+++ src/absences/absences.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AbsencesService } from './absences.service';
 import { AbsencesController } from './absences.controller';
+import { SupabaseModule } from '../supabase/supabase.module';
 
 @Module({
+    imports: [SupabaseModule],
     controllers: [AbsencesController],
     providers: [AbsencesService],
 })
